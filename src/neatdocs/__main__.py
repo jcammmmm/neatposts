ee055f289710805bb61df5ede6940ba61e96d939
Juan Camilo Florez <jcflorezv@unal.edu.co>
2022-09-11 21:47:51 -0500

    copying fonts to folder and python packaging

--- src/neatdocs/__main__.py
+++ src/neatdocs/__main__.py
@@ -1,12 +1,15 @@
 import sys
 import markdown as md
 import datetime as time
+import os
 from jinja2 import Environment, PackageLoader
+from fnmatch import fnmatch
+from importlib.resources import path
 from shutil import copytree
-
+from re import search, escape
 
 def main():
-  print("buajajajjajajajajajajaja!!!!!!!!!!!!!!!!!")
+  copy_fonts_and_ignore()
   if len(sys.argv) < 2:
     raise Exception('Please provide the markdown file name.')
 
@@ -15,7 +18,7 @@ def main():
   FONTS_LOC = 'fonts/cmuserif/'
 
   env = Environment(
-    loader=PackageLoader('webdoc', package_path='.', encoding='UTF-8'),
+    loader=PackageLoader('neatdocs', package_path='.', encoding='UTF-8'),
     autoescape=False
   )
 
@@ -37,11 +40,27 @@ def main():
 
 def copy_fonts_and_ignore():
   # check if fonts folder exists
+  copy = True
+  for file in os.listdir(os.getcwd()):
+    if fnmatch(file, 'fonts'):
+      copy = False
 
   # copy fonts
+  if copy:
+    fonts_src_path = str(path('neatdocs.fonts.cmuserif', ''))
+    fonts_des_path = os.path.join(os.getcwd(), 'fonts', 'cmuserif')
+    copytree(fonts_src_path, fonts_des_path)
 
   # edit .gitignore
-  pass
-
+  append = True
+  with open('.gitignore', 'r', encoding='UTF-8') as ignrfile:
+    for line in ignrfile:
+      if search(escape(line), 'fonts'):
+        append = False
+        break
+  with open('.gitignore', 'a', encoding='UTF-8') as ignrfile:
+    if append:
+      ignrfile.write('\nfonts')
+  
 if __name__ == '__main__':
   main()
